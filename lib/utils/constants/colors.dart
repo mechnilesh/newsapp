40fb790a048c1fd24c789650c88c2f61eae9a744
Mechnilesh <nilesh2001nk@gmail.com>
2023-03-09 20:15:27 +0530

    Headlines Page UI done

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
-const baseColor = Colors.black; //#000000
+const primaryColor = Colors.black; //#000000
 const secondaryColor = Color.fromRGBO(70, 70, 70, 1); //#464646
-const whiteColor = Colors.white; //#ffffff 
+const whiteColor = Colors.white; //#ffffff
+const headingFontColor = Color.fromRGBO(242, 242, 242, 1);
+const subHeadingFontColor = Color.fromRGBO(186, 186, 186, 1);
